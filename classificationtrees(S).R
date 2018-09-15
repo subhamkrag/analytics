af742a6534e31fc9075ec856cbd30ea8c74561fd
subhamkrag <subham1998kumar@gmail.com>
2018-09-15 10:06:19 +0530

    imp

---
+++ classificationtrees(S).R
@@ -0,0 +1,29 @@
+#Decision Tree
+
+library(rpart)
+library(rpart.plot)
+
+education=factor(c(3,2,2,3,2,2,3,2,2,2))#numerical like 3 is phd 2 is masters
+married=factor(c('S','M','S','M','M','M','S','S','M','S'))
+income=c(125,100,70, 120, 95, 60, 220, 85, 75, 90)
+buy=factor(c(0,0,0,0,1,0,0,1,0,1))
+(data= data.frame(buy, education, married, income))
+head(data)
+
+xtabs( ~ buy + education, data=data) #crosstabs are used for category not income
+
+table(data$buy, data$education)
+dtree1 = rpart(buy ~ education + married + income, data=data)#only root was created
+dtree1
+dtree1 = rpart(buy ~ education + married, data=data,  minsplit=4, minbucket=2,cp=-1)
+dtree1
+rpart.plot(dtree1, nn=T)
+#n=10 is rows
+printcp(dtree1)
+
+
+head(data)
+(ndata1 = data.frame(education=factor(2), married='S', income=110))
+predict(dtree1, newdata=ndata1, type='class')
+(p1=predict(dtree1, newdata=ndata1, type='prob'))
+cbind(ndata1, p1)
